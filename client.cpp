501758827be827820e94f7baa43dcbc120b8f8b7
kewinshi <k.jason.shi@gmail.com>
2023-09-25 04:29:56 +0000

    i am finished

--- client.cpp
+++ client.cpp
@@ -6,30 +6,32 @@
     Date: 2/8/20
 	
 	Please include your Name, UIN, and the date below
-	Name:
-	UIN:
-	Date:
+	Name: Kevin Shi
+	UIN: 831008207
+	Date: 09/12/2023
 */
 #include "common.h"
 #include "FIFORequestChannel.h"
 
 using namespace std;
 
-
 int main (int argc, char *argv[]) {
 	int opt;
-	int p = 1;
-	double t = 0.0;
-	int e = 1;
+	int p = -1; // setting the values to -1 for p, t, and e says that "there is no value".
+	double t = -1;
+	int e = -1;
+	int buff_size = MAX_MESSAGE;
+	bool newChanReq = false;
+	vector<FIFORequestChannel*> channels;
 	
 	string filename = "";
-	while ((opt = getopt(argc, argv, "p:t:e:f:")) != -1) {
+	while ((opt = getopt(argc, argv, "p:t:e:f:m:c")) != -1) {
 		switch (opt) {
 			case 'p':
-				p = atoi (optarg);
+				p = atoi (optarg); //atoi converts ASCII into int. stores value of the argument of the user to p.
 				break;
 			case 't':
-				t = atof (optarg);
+				t = atof (optarg); //optarg gets the argv?
 				break;
 			case 'e':
 				e = atoi (optarg);
@@ -37,34 +39,170 @@ int main (int argc, char *argv[]) {
 			case 'f':
 				filename = optarg;
 				break;
+			case 'm':
+				buff_size = atoi(optarg);
+				break;
+			case 'c':
+				newChanReq = true;
+				break;
+
 		}
 	}
 
-    FIFORequestChannel chan("control", FIFORequestChannel::CLIENT_SIDE);
-	
-	// example data point request
-    char buf[MAX_MESSAGE]; // 256
-    datamsg x(1, 0.0, 1);
-	
-	memcpy(buf, &x, sizeof(datamsg));
-	chan.cwrite(buf, sizeof(datamsg)); // question
-	double reply;
-	chan.cread(&reply, sizeof(double)); //answer
-	cout << "For person " << p << ", at time " << t << ", the value of ecg " << e << " is " << reply << endl;
-	
-    // sending a non-sense message, you need to change this
-	filemsg fm(0, 0);
-	string fname = "teslkansdlkjflasjdf.dat";
-	
-	int len = sizeof(filemsg) + (fname.size() + 1);
-	char* buf2 = new char[len];
-	memcpy(buf2, &fm, sizeof(filemsg));
-	strcpy(buf2 + sizeof(filemsg), fname.c_str());
-	chan.cwrite(buf2, len);  // I want the file length;
 
-	delete[] buf2;
+	// Part 1
+	pid_t server_request = fork();
+
+	if(server_request == 0)
+	{
+		char* argu [] = {(char*)("./server"), (char*)("-m"), (char*)std::to_string(buff_size).c_str(), nullptr};
+		execvp(argu[0], argu);
+	}
+
+	FIFORequestChannel cont_chan("control", FIFORequestChannel::CLIENT_SIDE);
+	channels.push_back(&cont_chan);
+
+	if (newChanReq) 
+	{
+		// send new channel request to the server
+		MESSAGE_TYPE nc = NEWCHANNEL_MSG;
+		cont_chan.cwrite(&nc, sizeof(MESSAGE_TYPE));
+		char buf0;
+		string chanName;
+		cont_chan.cread(&buf0, sizeof(char));
+		while (buf0 != '\0')
+		{
+			chanName.push_back(buf0);
+			cont_chan.cread(&buf0, sizeof(char));
+		}
+		cout << chanName;
+		FIFORequestChannel* new_chan = new FIFORequestChannel(chanName, FIFORequestChannel::CLIENT_SIDE);
+		cout << chanName;
+		channels.push_back(new_chan);
+	}
+	FIFORequestChannel chan = *(channels.back()); 
+
+	if (filename == "" && t == -1 && e == -1) // Part 2
+	{ 
+		ofstream file;
+		filename = "received/x1.csv";
+		file.open(filename);
+		double t = 0;
+		int count = 0;
+
+		while (count < 1000) {
+			file << t;
+			for (int i = 1; i <= 2; i++) { // x : 2 times : 1, 2
+				char buf[MAX_MESSAGE]; //256
+				datamsg x(p, t, i);
+				
+				memcpy(buf, &x, sizeof(datamsg));
+				chan.cwrite(buf, sizeof(datamsg)); 
+				double reply;
+				chan.cread(&reply, sizeof(double)); 
+				file << "," << reply;
+				// append "reply, " to x1.csv -> e = 1
+				// append "reply/n" to x1.csv -> e = 2
+			}
+			file << "\n";
+			t += 0.004;
+			count++;
+		}
+		// close x1.csv
+		file.close();
+	}
+	else if (filename != "")
+	{
+		// Receive the file length into filesize in bytes?
+		filemsg fm(0, 0); // parameters = file offset, length
+		int len = sizeof(filemsg) + (filename.size() + 1);
+		char* buf2 = new char[len]; // REQUEST BUFFER
+		char* buf3 = new char[buff_size]; // RESPONSE BUFFER: create buffer of size buff capacity(m)
+		memcpy(buf2, &fm, sizeof(filemsg));
+		strcpy(buf2 + sizeof(filemsg), filename.c_str());
+		chan.cwrite(buf2, len);  // I want the file length;
+		int64_t filesize = 0;
+		chan.cread(&filesize, sizeof(int64_t));
 	
+		// Create file in /received/filename
+		ofstream ofile;
+		ofile.open("received/" + filename);
+		if (!ofile.is_open())
+		{
+			cout << "Could not open file" << endl;
+			return -1;
+		}
+		// cout << "buff size is: " << buff_size << endl;
+		// cout << "file length is: " << filesize << endl;
+		// loop over the segments in the file: filesize / buff capacity
+		int loop_count = floor((double)filesize / (double)buff_size);
+		//cout << "loop count = " << loop_count << endl;
+		for (int i = 0; i < loop_count; i++)
+		{
+			// create filemsg instance
+			filemsg* file_req = (filemsg*)buf2;
+			// set offset in the file
+			file_req->offset = buff_size * i;
+
+			// set the length. be careful of the last segment.
+			file_req->length = buff_size;
+			// send the request (buf2)
+			chan.cwrite(buf2, len);
+
+			// receive the response
+			// cread into buf3 length file_req->len ???
+			chan.cread(buf3, file_req->length);
+
+			// write buf3 into file: received/filename.
+			ofile.write(buf3, buff_size);
+
+			// update remaining bytes and loop again
+		}
+		// create filemsg instance
+		filemsg* file_req = (filemsg*)buf2;
+		// set offset in the file
+		file_req->offset = buff_size * loop_count;
+
+		// set the length. be careful of the last segment.
+		file_req->length = filesize % buff_size;
+		// send the request (buf2)
+		chan.cwrite(buf2, len);
+
+		// receive the response
+		// cread into buf3 length file_req->len ???
+		chan.cread(buf3, file_req->length);
+
+		// write buf3 into file: received/filename.
+		ofile.write(buf3, filesize % buff_size);
+
+		// update remaining bytes and loop again
+
+		delete[] buf2;
+		delete[] buf3;
+	}
+	else 
+	{
+		char buf[MAX_MESSAGE]; // 256
+    	//datamsg x(1, 0.0, 1); // change from hardcoding to user's values
+		datamsg x(p, t, e);
+		memcpy(buf, &x, sizeof(datamsg));
+		chan.cwrite(buf, sizeof(datamsg)); // question // writing reply
+		double reply;
+		chan.cread(&reply, sizeof(double)); //answer // the server reads this reply. prints double reply.
+		cout << "For person " << p << ", at time " << t << ", the value of ecg " << e << " is " << reply << endl;
+	}
+
+	
+	if (newChanReq) 
+	{
+		MESSAGE_TYPE m = QUIT_MSG;
+    	chan.cwrite(&m, sizeof(MESSAGE_TYPE));
+		delete channels.back();
+		chan = *(channels.front());
+	}
+
 	// closing the channel    
     MESSAGE_TYPE m = QUIT_MSG;
     chan.cwrite(&m, sizeof(MESSAGE_TYPE));
+
 }
--- pa1-tests.sh
+++ pa1-tests.sh
@@ -14,6 +14,15 @@ checkclean () {
             #echo "Failed to close FIFORequestChannel - removing for next test"
             find . -type p -exec rm {} +
         fi
+    elif [ "$1" = "l" ]; then
+        sleep 5
+        if [ "$(find . -type p)" ]; then
+            echo -e "\nFailed to close FIFORequestChannel"
+            echo -e "  ${RED}Failed${NC}"
+        else
+            echo -e "  ${GREEN}Test Thirteen Passed${NC}"
+            SCORE=$(bc -l <<< "$SCORE+5")
+        fi
     else
         echo "something broke"
         exit 1
@@ -35,46 +44,57 @@ else
     NC='\033[0m'
 fi
 
+SCORE=0
+echo "${SCORE}" > ../file.txt
+
 
 remake
 echo -e "\nVerify the server runs as child\n"
 timeout 5 strace -e trace=execve -f -o trace.tst ./client -p 1 -t 0.004 -e 1 >/dev/null 2>&1
 if ! grep -q 'execve("./server"' trace.tst; then
     echo "  Server does not run as child - can't test with script"
+    echo -e "  ${RED}Failed${NC}"
     exit 1
+else
+    echo -e "  ${GREEN}Test One Passed${NC}"
+    SCORE=$(bc -l <<< "$SCORE+15")
 fi
 
 
 remake
-#echo -e "\nTest cases for single datapoint transfers"
+echo -e "\nTest cases for single datapoint transfers"
 
+#Single datapoint test (1)
 echo -e "\nTesting :: ./client -p 12 -t 0.016 -e 1\n"
 VAL=$(awk -F, '$1 == "0.016" { print $2 }' BIMDC/12.csv)
 if ./client -p 12 -t 0.016 -e 1 | grep -qe ${VAL}; then
-    echo -e "  ${GREEN}Passed${NC}"
+    echo -e "  ${GREEN}Test Two Passed${NC}"
+    SCORE=$(bc -l <<< "$SCORE+3")
 else
     echo -e "  ${RED}Failed${NC}"
 fi
 checkclean "f"
 
+#Single datapoint test (2)
 echo -e "\nTesting :: ./client -p 5 -t 59.996 -e 2\n"
 VAL=$(awk -F, '$1 == "59.996" { print $3 }' BIMDC/5.csv)
 if ./client -p 5 -t 59.996 -e 2 | grep -qe ${VAL}; then
-    echo -e "  ${GREEN}Passed${NC}"
+    echo -e "  ${GREEN}Test Three Passed${NC}"
+    SCORE=$(bc -l <<< "$SCORE+4")
 else
     echo -e "  ${RED}Failed${NC}"
 fi
 checkclean "f"
 
-
 remake
-#echo -e "\nTest cases for multiple datapoint transfers"
 
+#Multiple datapoint test (1)
 echo -e "\nTesting :: ./client -p 9; diff -sqwB <(head -n 1000 BIMDC/9.csv) received/x1.csv\n"
 ./client -p 9 >/dev/null 2>&1
 if test -f "received/x1.csv"; then
     if diff -wB <(head -n 1000 BIMDC/9.csv) received/x1.csv >/dev/null; then
-        echo -e "  ${GREEN}Passed${NC}"
+        echo -e "  ${GREEN}Test Four Passed${NC}"
+        SCORE=$(bc -l <<< "$SCORE+4")
     else
         echo -e "  ${RED}Failed${NC}"
     fi
@@ -83,11 +103,13 @@ else
 fi
 checkclean "f"
 
+#Multiple datapoint test (2)
 echo -e "\nTesting :: ./client -p 5; diff -sqwB <(head -n 1000 BIMDC/5.csv) received/x1.csv\n"
 ./client -p 5 >/dev/null 2>&1
 if test -f "received/x1.csv"; then
     if diff -wB <(head -n 1000 BIMDC/5.csv) received/x1.csv >/dev/null; then
-        echo -e "  ${GREEN}Passed${NC}"
+        echo -e "  ${GREEN}Test Five Passed${NC}"
+        SCORE=$(bc -l <<< "$SCORE+4")
     else
         echo -e "  ${RED}Failed${NC}"
     fi
@@ -96,15 +118,16 @@ else
 fi
 checkclean "f"
 
-
 remake
 # echo -e "\nTest cases for CSV file transfers (with default buffer capacity)"
 
+#CSV file transfer test (1)
 echo -e "\nTesting :: ./client -f 8.csv; diff BIMDC/8.csv received/8.csv\n"
 ./client -f 8.csv >/dev/null 2>&1
 if test -f "received/8.csv"; then
     if diff BIMDC/8.csv received/8.csv >/dev/null; then
-        echo -e "  ${GREEN}Passed${NC}"
+        echo -e "  ${GREEN}Test Six Passed${NC}"
+        SCORE=$(bc -l <<< "$SCORE+6")
     else
         echo -e "  ${RED}Failed${NC}"
     fi
@@ -113,11 +136,13 @@ else
 fi
 checkclean "f"
 
+#CSV file transfer test (2)
 echo -e "\nTesting :: ./client -f 2.csv; diff BIMDC/2.csv received/2.csv\n"
 ./client -f 2.csv >/dev/null 2>&1
 if test -f "received/2.csv"; then
     if diff BIMDC/2.csv received/2.csv >/dev/null; then
-        echo -e "  ${GREEN}Passed${NC}"
+        echo -e "  ${GREEN}Test Seven Passed${NC}"
+        SCORE=$(bc -l <<< "$SCORE+7")
     else
         echo -e "  ${RED}Failed${NC}"
     fi
@@ -128,13 +153,14 @@ checkclean "f"
 
 
 remake
-# echo -e "\nTest cases for CSV file transfers (with varying buffer capacity)"
-
+echo -e "\nTest cases for CSV file transfers (with varying buffer capacity)"
+#CSV file transfer test (3)
 echo -e "\nTesting :: ./client -f 11.csv -m 512; diff BIMDC/11.csv received/11.csv\n"
 ./client -f 11.csv -m 512 >/dev/null 2>&1
 if test -f "received/11.csv"; then
     if diff BIMDC/11.csv received/11.csv >/dev/null; then
-        echo -e "  ${GREEN}Passed${NC}"
+        echo -e "  ${GREEN}Test Eight Passed${NC}"
+        SCORE=$(bc -l <<< "$SCORE+7")
     else
         echo -e "  ${RED}Failed${NC}"
     fi
@@ -147,12 +173,14 @@ checkclean "f"
 remake
 # echo -e "\nTest cases for binary file transfers (with default buffer capacity)"
 
+#Binary file transfer test (1)
 echo -e "\nTesting :: truncate -s 256K BIMDC/test.bin; ./client -f test.bin; diff BIMDC/test.bin received/test.bin\n"
 truncate -s 256K BIMDC/test.bin
 ./client -f test.bin >/dev/null 2>&1
 if test -f "received/test.bin"; then
     if diff BIMDC/test.bin received/test.bin >/dev/null; then
-        echo -e "  ${GREEN}Passed${NC}"
+        echo -e "  ${GREEN}Test Nine Passed${NC}"
+        SCORE=$(bc -l <<< "$SCORE+5")
     else
         echo -e "  ${RED}Failed${NC}"
     fi
@@ -162,15 +190,18 @@ fi
 checkclean "f"
 
 
+
 remake
 # echo -e "\nTest cases for binary file transfers (with varying buffer capacity)"
 
+#Binary file transfer test (2)
 echo -e "\nTesting :: truncate -s 256K BIMDC/test.bin; ./client -f test.bin -m 512; diff BIMDC/test.bin received/test.bin\n"
 truncate -s 256K BIMDC/test.bin
 ./client -f test.bin -m 512 >/dev/null 2>&1
 if test -f "received/test.bin"; then
     if diff BIMDC/test.bin received/test.bin >/dev/null; then
-        echo -e "  ${GREEN}Passed${NC}"
+        echo -e "  ${GREEN}Test Ten Passed${NC}"
+        SCORE=$(bc -l <<< "$SCORE+5")
     else
         echo -e "  ${RED}Failed${NC}"
     fi
@@ -183,11 +214,13 @@ checkclean "f"
 remake
 # echo -e "\nTest cases for new channel"
 
+#New channel test (1)
 echo -e "\nTesting :: ./client -c -p 12 -t 0.016 -e 1\n"
 VAL=$(awk -F, '$1 == "0.016" { print $2 }' BIMDC/12.csv)
 if strace -e trace=openat -o trace.tst ./client -c -p 12 -t 0.016 -e 1 2>/dev/null | grep -qe ${VAL}; then
     if grep -q 'openat(AT_FDCWD, "fifo_data' trace.tst; then
-        echo -e "  ${GREEN}Passed${NC}"
+        echo -e "  ${GREEN}Test Eleven Passed${NC}"
+        SCORE=$(bc -l <<< "$SCORE+7")
     else
         echo -e "  ${ORANGE}No new channel opened${NC}"
     fi
@@ -196,12 +229,14 @@ else
 fi
 checkclean "f"
 
+#New channel test (2)
 echo -e "\nTesting :: ./client -c -f 10.csv -m 1024; diff BIMDC/10.csv received/10.csv\n"
 strace -e trace=openat -o trace.tst ./client -c -f 10.csv -m 1024 >/dev/null 2>&1
 if test -f "received/10.csv"; then
     if diff BIMDC/10.csv received/10.csv >/dev/null; then
         if grep -q 'openat(AT_FDCWD, "fifo_data' trace.tst; then
-            echo -e "  ${GREEN}Passed${NC}"
+            echo -e "  ${GREEN}Test Twelve Passed${NC}"
+            SCORE=$(bc -l <<< "$SCORE+8")
         else
             echo -e "  ${ORANGE}No new channel opened${NC}"
         fi
@@ -211,8 +246,12 @@ if test -f "received/10.csv"; then
 else
     echo -e "  ${ORANGE}No 10.csv in received/ directory${NC}"
 fi
-checkclean "f"
+checkclean "l"
 
+
+
+echo -e "\nSCORE: ${SCORE}/80\n"
+echo "${SCORE}" > ../file.txt
 make -s clean
 echo -e "\n"
 exit 0
